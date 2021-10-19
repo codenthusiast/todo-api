a5251f4c973a0ec6b1793cd722b54247cb44daf7
Gafar Popoola <popoola.gafar.babatunde@gmail.com>
2021-10-19 22:57:07 +0100

    allowed cross origins

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -79,6 +79,11 @@ namespace TodoApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials()); // allow credentials
 
             app.UseAuthorization();
 
