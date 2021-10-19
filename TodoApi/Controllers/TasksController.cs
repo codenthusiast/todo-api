22814e91b62e5ed8ef1933517ca10365ff376655
Gafar Popoola <gafar.popoola@venturegardengroup.com>
2021-10-19 15:00:57 +0100

    Implemented Task controller and User controller

---
+++ TodoApi/Controllers/TasksController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Core.Entities;
+using TodoApi.Core.Interfaces;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly ITaskService _taskService;
+
+        public TasksController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        // GET: api/<TasksController>
+        [HttpGet]
+        public async Task<IEnumerable<UserTask>> Get()
+        {
+            var users = await _taskService.GetAllTasks();
+            return users;
+        }
+
+        // GET api/<TasksController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserTask>> Get(int id)
+        {
+            var user = await _taskService.GetTask(id);
+            if (user == null)
+            {
+                return NotFound("Task not found");
+            }
+            return Ok(user);
+        }
+
+        // POST api/<TasksController>
+        [HttpPost]
+        public async Task<ActionResult<UserTask>> Post([FromBody] UserTask user)
+        {
+            var newUserTask = await _taskService.CreateTask(user);
+            return CreatedAtAction(nameof(Get), new { id = newUserTask.Id });
+        }
+
+        // PUT api/<TasksController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] UserTask user)
+        {
+            var userToUpdate = await _taskService.GetTask(id);
+            if (userToUpdate == null)
+            {
+                return NotFound("Task not found");
+            }
+            await _taskService.UpdateTask(user);
+            return NoContent();
+        }
+
+        // DELETE api/<TasksController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var userToUpdate = await _taskService.GetTask(id);
+            if (userToUpdate == null)
+            {
+                return NotFound("Task not found");
+            }
+            await _taskService.DeleteTask(id);
+            return NoContent();
+        }
+    }
+}
--- TodoApi/Controllers/UsersController.cs
+++ TodoApi/Controllers/UsersController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TodoApi.Core.Entities;
+using TodoApi.Core.Interfaces;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +14,65 @@ namespace TodoApi.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
         // GET: api/<UsersController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IEnumerable<User>> Get()
         {
-            return new string[] { "value1", "value2" };
+            var users = await _userService.GetAllUsers();
+            return users;
         }
 
         // GET api/<UsersController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<User>> Get(int id)
         {
-            return "value";
+            var user = await _userService.GetUserById(id);
+            if(user == null)
+            {
+                return NotFound("User not found");
+            }
+            return Ok(user);
         }
 
         // POST api/<UsersController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<User>> Post([FromBody] User user)
         {
+            var newUser = await _userService.CreateUser(user);
+            return CreatedAtAction(nameof(Get), new { id = newUser.Id});
         }
 
         // PUT api/<UsersController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<ActionResult> Put(int id, [FromBody] User user)
         {
+            var userToUpdate = await _userService.GetUserById(id);
+            if(userToUpdate == null)
+            {
+                return NotFound();
+            }
+            await _userService.UpdateUser(user);
+            return NoContent();
         }
 
         // DELETE api/<UsersController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
+            var userToUpdate = await _userService.GetUserById(id);
+            if (userToUpdate == null)
+            {
+                return NotFound();
+            }
+            await _userService.DeleteUser(id);
+            return NoContent();
         }
     }
 }
